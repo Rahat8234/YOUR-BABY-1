6c26e132a581dad16e742b8607d91ba613e78210
RAHATKHAN8234 <rifatrahatkhan@gmail.com>
2024-08-24 00:44:35 +0600

    Update and rename 500 CYBER ✌️.js to ✌️.js

--- scripts/commands/✌️.js
+++ scripts/commands/✌️.js
@@ -11,7 +11,7 @@ module.exports.config = {
   name: "✌️",
   version: "1.0.0",
   permssion: 0,
-  credits: "Islamick Cyber Chat",
+  credits: "𝐊𝐡𝐚𝐧 𝐑𝐚𝐡𝐮𝐥 𝐑𝐊",
   prefix:true,
   description: "auto reply to salam",
   category: "noprefix",
